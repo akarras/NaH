d324389fded168dad463b97a6fbacce4af3c5338
Aaron Karras <chw111111@hotmail.com>
2016-05-20 00:52:00 -0600

    Removed unused commands used for testing

--- NaH-Bukkit/src/main/java/net/supernoobs/nah/Nah.java
+++ NaH-Bukkit/src/main/java/net/supernoobs/nah/Nah.java
@@ -44,57 +44,9 @@ public class Nah extends JavaPlugin {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(label.equalsIgnoreCase("nah")) {
 			if(sender instanceof Player) {
-				if(args.length > 0) {
-					if(args[0].equalsIgnoreCase("create")) {
-						User user = userManager.getUser((Player) sender);
-						gameManager.createGame(user);
-						sender.sendMessage("§aGame created!");
-						return true;
-					}
-					else if(args[0].equalsIgnoreCase("join")) {
-						if(args.length > 1) {
-							User user = userManager.getUser((Player) sender);
-							Game game = gameManager.getGame(args[1]);
-							if(game == null) {
-								sender.sendMessage(String.format("§4%s not found!",args[1]));
-								return true;
-							}
-							game.joinGame(user);
-							sender.sendMessage("§aSuccessful joined the game!");
-							return true;
-						}
-						
-					} else if(args[0].equalsIgnoreCase("leave")) {
-						User user = userManager.getUser((Player) sender);
-						user.getGame().quitGame(user);
-						sender.sendMessage("§cLeft Game!");
-						return true;
-					} else if(args[0].equalsIgnoreCase("start")) {
-						User user = userManager.getUser((Player) sender);
-						if(user.isHost()) {
-							user.getGame().start();
-							sender.sendMessage("§aGame has started");
-							return true;
-						} else {
-							sender.sendMessage("§cOnly the host may start the game");
-							return true;
-						}
-					}
-					else if(args[0].equalsIgnoreCase("printcards")) {
-						User user = userManager.getUser((Player) sender);
-						//sender.sendMessage(user.getGame().getWhiteCards().drawCard().getText());
-						sender.sendMessage("Black:"+user.getGame().getCurrentBlackCard().getText());
-						for(WhiteCard card:user.getHand()) {
-							sender.sendMessage(card.getText());
-						}
-						return true;
-					}
-					
-				} else {
-					User user = userManager.getUser((Player) sender);
-					user.updateGUI();
-					return true;
-				}
+				User user = userManager.getUser((Player) sender);
+				user.updateGUI();
+				return true;
 			}else {
 				sender.sendMessage("§cThis command must be run from the console");
 			}
